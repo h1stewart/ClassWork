656fcd677d3d7b6bb835be11399ae290d45f3b93
Hailey Stewart <kickinhailey@icloud.com>
2023-01-30 17:23:31 -0500

    Elvis App

--- TwoButtonChallenge/ContentView.swift
+++ TwoButtonChallenge/ContentView.swift
@@ -8,19 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString = ""
+    @State private var imagePic = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.purple)
+                .padding()
+            Spacer()
+            
+            Image(systemName: imagePic)
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.black)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.purple)
+            
+            Spacer()
+            HStack{
+                Button("Peace") {
+                    imagePic = "peacesign"
+                    messageString = "Peace"
+                }
+                Button("Love") {
+                    imagePic = "heart"
+                    messageString = "Love"
+                }
+                Button("Understanding") {
+                    imagePic = "lightbulb"
+                    messageString = "Understanding"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
