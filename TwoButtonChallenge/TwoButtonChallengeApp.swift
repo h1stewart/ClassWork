6adca5cf01edb23086cebf37d91d634dc23c54a7
Hailey Stewart <kickinhailey@icloud.com>
2023-01-30 16:35:31 -0500

    Initial Commit

---
+++ TwoButtonChallenge/TwoButtonChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoButtonChallengeApp.swift
+//  TwoButtonChallenge
+//
+//  Created by Hailey Stewart on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TwoButtonChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
